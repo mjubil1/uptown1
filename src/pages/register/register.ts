9717a323ad4e39f2b32fc2b3566ba9ef8ba238e6
Montrell Jubilee <mjubil1@students.towson.edu>
2017-12-22 15:16:53 -0500

    Testing registration page, that values are logged to the console correctly

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,16 +1,8 @@
-import { Component, /*ViewChild*/ } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
 //import { AngularFireAuth } from 'angularfire2/auth';
-
 //import { LoginPage } from '../login/login';
-
-
-/**
- * Generated class for the RegisterPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+//import { NgForm } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -18,40 +10,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'register.html',
 })
 export class RegisterPage {
-  
-  //Variables
-  fName: string;
-  lName: string;
-  email: string;
-  password: string;
 
-  data = {};
-  genders = ['Male', 'Female'];
-
-  constructor(/*private fire: AngularFireAuth,*/
-              public navCtrl: NavController, 
-              public navParams: NavParams,
-              ) 
-  {}
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
+  onSubmit(form: any): void {
+    console.log('DATA',form.value);
   }
-
-  //ViewChild - To get access to a component and its methods, we can use @ViewChild decorator.
-  //@ViewChild('email') email;
-  //@ViewChild('password') password;
-  
-
-  registerUser() {
-    this.fName;
-    this.lName;
-    this.email;
-    this.password;
-
-    console.log(this.data);
-    //this.fire.auth.createUserWithEmailAndPassword();
-
-    }
 }
 
