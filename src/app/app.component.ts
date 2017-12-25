54628057a66e4408ba4880f9781a8f57c0a639cc
Montrell Jubilee <mjubil1@students.towson.edu>
2017-12-25 02:49:44 -0500

    Added side-menu and navigation to other pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,36 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, NavController, MenuController } from 'ionic-angular';
 import firebase from 'firebase';
 import { LoginPage } from '../pages/login/login';
 import { Splashscreen } from 'ionic-native';
 import { HomePage } from '../pages/home/home';
 import { PaginationPage } from '../pages/pagination/pagination';
+import { RedeemPage } from '../pages/redeem/redeem';
+import { SettingPage } from '../pages/setting/setting';
 
 @Component({
   templateUrl: 'app.html'
 })          
 export class MyApp {
 
-  rootPage:any = PaginationPage;  
+  rootPage:any = HomePage;  
+  homePage = HomePage;
+  redeemPage = RedeemPage;
+  settingPage = SettingPage;
 
-  constructor(platform: Platform) 
+  @ViewChild('nav') nav: NavController;
+
+  constructor(platform: Platform, private menuCtrl: MenuController) 
   {
     platform.ready().then(() => 
     {
       Splashscreen.hide();
     });
+  }
+
+  onLoad(page:any) {
+    this.nav.setRoot(page);
+    this.menuCtrl.close();
   }
 
 
