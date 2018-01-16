08d4d8f21728ff0942d1a3ca007a20260ae155d5
davidhanlon23 <davidhanlon23@gmail.com>
2018-01-15 22:33:21 -0500

    Resizing favorite move's chart

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage,NavParams } from 'ionic-angular';
 import { ChartsModule, Color } from 'ng2-charts';
+import { Chart } from 'chart.js';
 
 @IonicPage()
 @Component({
@@ -11,17 +12,21 @@ export class HomePage {
 
   @ViewChild('pointChart') pointChart: ElementRef;
   @ViewChild('favMoveChart') favMoveChart: ElementRef;
-  count = 50;
+  count = 100;
   max = 100;
   
   //Data for user's charts 
   data:number[] = [this.count, (this.max-this.count)];
-  data3: number[] = [11,23,32,43,9];
+  data3: number[] = [93,76,60,47,36];
   type:string = 'doughnut';
   type3:string = 'horizontalBar';
 
   colorsOverride: Array<Color> = [{
     backgroundColor: ["rgba(252,215,49, 0.82)", "transparent"],
+  }];
+
+  colorsOverride3: Array<Color> = [{
+    backgroundColor: ["rgba(252,215,49, 0.82)", "rgba(252,215,49, 0.82)","rgba(252,215,49, 0.82)","rgba(252,215,49, 0.82)","rgba(252,215,49, 0.82)"],
   }];
 
   favMoveLabels = ['Rec Room','Green Turtle','CVP','B-Lounge','Torrent'];
@@ -49,10 +54,19 @@ export class HomePage {
     }
   }
 
+  favMoveChartOptions: any = {
+    maintainAspectRatio: false,
+    legend: {
+      display: false
+    },
+    scales: {
+      yAxes: [{ barThickness: 33 }]
+    }
+  }
+
   favMoveDatasets: any[] = [
   {
-    data: this.data3 
+    data: this.data3
   }
   ]
-
 }
